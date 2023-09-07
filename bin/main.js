4a76ba807e78b69bc5001d21d1421e36cb76707b
Prarup Gurung <grgprarup@gmail.com>
2023-09-07 10:35:07 +0545

    Entrypoint to library: bin/main.js

---
+++ bin/main.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+import startServer from '../lib/server'
+import open from 'open'
+import argsParser from 'yargs-parser'
+import fs from 'fs-extra'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+import * as path from 'path'
+
+const alias = {
+    h: 'help',
+    s: 'separator',
+    S: 'vertical-separator'
+}
+const argv = argsParser(process.argv.slice(2), { alias })
+console.log(argv)
+const hasPath = Boolean(argv._[0])
+
+;(async () => {
+    if (hasPath) {
+        let server, initialUrl
+
+        try {
+            ;[server, initialUrl] = await startServer()
+            console.log(`The slides are at ${initialUrl}`)
+            !disableAutoOpen && await open(initialUrl, {url: true})
+            process.on('SIGINT', () => {
+                console.log('Received SIGINT, closing gracefully.')
+                server.close()
+                process.exit(128)
+            })
+        } catch (err) {
+            console.error(err)
+            process.exit(1)
+        }
+    } else {
+        const currentDir = dirname(fileURLToPath(import.meta.url))
+        console.log(currentDir)
+        const help = await fs.readFile(path.join(currentDir, './help.txt'))
+        console.log(help.toString())
+    }
+})()
